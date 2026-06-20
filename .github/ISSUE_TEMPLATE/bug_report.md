9bb44727ff63bbf5f2759fe509be15063a36719a
signal <83561323+si6nal@users.noreply.github.com>
2026-06-20 10:01:48 -0600

    removed & updated old files

--- .github/ISSUE_TEMPLATE/bug_report.md
+++ .github/ISSUE_TEMPLATE/bug_report.md
@@ -3,7 +3,7 @@ name: Bug report
 about: Create a report to help improve Ambien
 title: ''
 labels: ''
-assignees: iiiiiiiris
+assignees: si6nal
 
 ---
 
--- src/main/java/me/iris/ambien/obfuscator/exceptions/SettingConflictException.java
+++
@@ -1,7 +0,0 @@
-package me.iris.ambien.obfuscator.exceptions;
-
-public class SettingConflictException extends RuntimeException {
-    public SettingConflictException(final String str) {
-        super(str);
-    }
-}
